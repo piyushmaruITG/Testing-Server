c35c068d8ad80f9c1ea8f2578eddeb53aae7da8e
piyushmaruITG <piyushmaru@itgeeks.com>
2025-03-11 18:00:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let express = require('express');
 let app = express();
+const Shopify = require('shopify-api-node');
 
-app.get('/',(request,response)=>{
-    response.send('Server Started');
+const shopify = new Shopify({
+    shopName: 'your-shop-name',
+    apiKey: '76f4eb6798615f02a63a9a19e0f4f946',
+    password: 'your-app-password'
+  });
+
+app.post('/update-product',(request,response)=>{
+    try{
+        const { variantId, newPrice } = request.body;
+        shopify.productVariant.update(variantId, { price: newPrice });
+    }catch(error){
+        console.log(error);
+    }
 })
+
 
 app.listen(8000, () => console.log('Server started on http://localhost:8000'));
